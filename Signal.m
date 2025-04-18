5e5f8405ff65ee936fc4e8c725f09a3e5a77c4b0
Ahmad Ebreeq <ahmdb625@gmail.com>
2025-04-18 16:22:22 +0300

    Create Signal.m

---
+++ Signal.m
@@ -0,0 +1,52 @@
+Fs = 44100;
+image = imread('imageImported.jpg');
+image = imresize(image, [32, 32]);
+image = double(image);
+image = image / max(image(:));
+[rows, cloumns];
+
+scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
+numNotes = length(scale);
+t = 0:1/Fs:0.2;
+musicSignal = [];
+
+for r = 1:rows
+    rowSignal = zeros(size(t));
+    for c = 1:columns
+        noteIdx = mod(c-1, numNotes) + 1;
+        freq = scale(noteIdx);
+        amp = image(r, c);
+        rowSignal = rowSignal + amp * sin(2*pi*freq*t);
+    end
+    musicSignal = [musicSignal, rowSignal];
+end
+
+musicSignal = musicSignal / max(abs(musicSignal));
+Sound(musicSignal, Fs);
+audiowrite('image_to_music.wav', musicSignal, Fs);
+
+figure;
+spectrogram(musicSignal, 256, 250, 256, Fs, 'yaxis');
+title('Spectrogram of Image-Music Signal');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
