67760181b326e95bd4ba115a2daf3a1b6d03a254
Ahmad Ebreeq <ahmdb625@gmail.com>
2025-04-18 20:29:17 +0300

    Update Signal.m

--- Signal.m
+++ Signal.m
@@ -1,15 +1,15 @@
 Fs = 44100;
 image = imread('imageImported.jpg');
-image = imresize(image, [32, 32]);
+image = imresize(image, [32 32]);
 image = double(image);
 image = image / max(image(:));
-[rows, cloumns];
+[rows, columns] = size(image);
 
 scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
 numNotes = length(scale);
 t = 0:1/Fs:0.2;
-musicSignal = [];
 
+musicSignal = [];
 for r = 1:rows
     rowSignal = zeros(size(t));
     for c = 1:columns
